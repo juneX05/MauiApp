98e13eec8336e449731ab5196e826b406bc0f98a
JuneX05 <joelvankibona@gmail.com>
2023-02-18 16:25:28 +0300

    Navigation Added

--- MauiApp1/ViewModel/MainViewModel.cs
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -39,5 +39,11 @@ namespace MauiApp1.ViewModel
                 Items.Remove(s);
             }
         }
+
+        [RelayCommand]
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        }
     }
 }
